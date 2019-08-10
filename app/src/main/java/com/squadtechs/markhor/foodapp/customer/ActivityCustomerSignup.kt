12e4729c023619069d733bc47f25ccb4fa3fd3cb
markhor <mohammadfaizan980@gmail.com>
2019-08-10 10:27:30 +0500

    Added empty trader login, sign up and customer sign up activities

---
+++ app/src/main/java/com/squadtechs/markhor/foodapp/customer/ActivityCustomerSignup.kt
@@ -0,0 +1,13 @@
+package com.squadtechs.markhor.foodapp.customer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.squadtechs.markhor.foodapp.R
+
+class ActivityCustomerSignup : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_signup)
+    }
+}
