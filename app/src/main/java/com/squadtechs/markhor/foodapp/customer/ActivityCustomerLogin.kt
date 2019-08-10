60f5c66c4dcfe5f8888d1b1644057379a63a75f1
markhor <mohammadfaizan980@gmail.com>
2019-08-10 11:46:13 +0500

    Added customer sign up XML

--- app/src/main/java/com/squadtechs/markhor/foodapp/customer/ActivityCustomerLogin.kt
+++ app/src/main/java/com/squadtechs/markhor/foodapp/customer/ActivityCustomerLogin.kt
@@ -1,14 +1,30 @@
 package com.squadtechs.markhor.foodapp.customer
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.WindowManager
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.squadtechs.markhor.foodapp.R
 
 class ActivityCustomerLogin : AppCompatActivity() {
 
+    private lateinit var linearNoAccount: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customer_login)
+        initViews()
+        setListeners()
     }
+
+    private fun setListeners() {
+        linearNoAccount.setOnClickListener {
+            startActivity(Intent(this, ActivityCustomerSignup::class.java))
+        }
+    }
+
+    private fun initViews() {
+        linearNoAccount = findViewById(R.id.linear_customer_no_account)
+    }
+
 }
