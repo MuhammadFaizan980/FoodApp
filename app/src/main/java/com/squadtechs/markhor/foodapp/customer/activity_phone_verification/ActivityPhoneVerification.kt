ef96efde4b03adf0b554ad21b13f3b62027fc1fc
markhor <mohammadfaizan980@gmail.com>
2019-08-29 15:27:59 +0500

    Added phone verification screen XML UI code

---
+++ app/src/main/java/com/squadtechs/markhor/foodapp/customer/activity_phone_verification/ActivityPhoneVerification.kt
@@ -0,0 +1,13 @@
+package com.squadtechs.markhor.foodapp.customer.activity_phone_verification
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.squadtechs.markhor.foodapp.R
+
+class ActivityPhoneVerification : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phone_verification)
+    }
+}
