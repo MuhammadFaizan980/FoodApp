5b527860c2eb24d9ff26f214ea0171aad491cacf
faizan <mohammadfaizan980@gmail.com>
2019-09-04 22:20:47 +0500

    Added home fragment XML code

--- app/src/main/java/com/squadtechs/markhor/foodapp/customer/customer_fragment_home/CustomerFragmentHome.kt
+++ app/src/main/java/com/squadtechs/markhor/foodapp/customer/customer_fragment_home/CustomerFragmentHome.kt
@@ -2,24 +2,20 @@ package com.squadtechs.markhor.foodapp.customer.customer_fragment_home
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
 import com.squadtechs.markhor.foodapp.R
 
-/**
- * A simple [Fragment] subclass.
- */
 class CustomerFragmentHome : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.customer_fragment_home, container, false)
+        val view = inflater.inflate(R.layout.customer_fragment_home, container, false)
+        return view
     }
 
 
