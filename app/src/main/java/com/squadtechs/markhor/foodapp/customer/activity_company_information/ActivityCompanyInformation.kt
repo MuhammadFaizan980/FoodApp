9a91ab3126121f361d2bc7566810fe26f094c067
faizan <mohammadfaizan980@gmail.com>
2019-09-13 16:18:10 +0500

    Added company information UI

---
+++ app/src/main/java/com/squadtechs/markhor/foodapp/customer/activity_company_information/ActivityCompanyInformation.kt
@@ -0,0 +1,13 @@
+package com.squadtechs.markhor.foodapp.customer.activity_company_information
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.squadtechs.markhor.foodapp.R
+
+class ActivityCompanyInformation : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_company_information)
+    }
+}
