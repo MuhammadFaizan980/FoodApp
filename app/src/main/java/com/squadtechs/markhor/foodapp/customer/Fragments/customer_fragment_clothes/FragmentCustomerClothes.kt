6401b44bdfa147ab167b4159f26c4bf083e2d539
faizan <mohammadfaizan980@gmail.com>
2019-09-16 12:02:07 +0500

    Added clothes row design

--- app/src/main/java/com/squadtechs/markhor/foodapp/customer/Fragments/customer_fragment_clothes/FragmentCustomerClothes.kt
+++ app/src/main/java/com/squadtechs/markhor/foodapp/customer/Fragments/customer_fragment_clothes/FragmentCustomerClothes.kt
@@ -2,21 +2,20 @@ package com.squadtechs.markhor.foodapp.customer.Fragments.customer_fragment_clot
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
 import com.squadtechs.markhor.foodapp.R
 
-/**
- * A simple [Fragment] subclass.
- */
 class FragmentCustomerClothes : Fragment() {
+    private lateinit var mView: View
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_customer_clothes, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mView = inflater.inflate(R.layout.fragment_customer_clothes, container, false)
+        return mView
     }
 }
