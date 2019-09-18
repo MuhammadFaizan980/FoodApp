6f6eeefff91b5896a7c8fe009e52661f93c73844
faizan <mohammadfaizan980@gmail.com>
2019-09-18 15:46:51 +0500

    Added customer addresses UI

---
+++ app/src/main/java/com/squadtechs/markhor/foodapp/customer/activity_customer_address/ActivityCustomerAddress.kt
@@ -0,0 +1,13 @@
+package com.squadtechs.markhor.foodapp.customer.activity_customer_address
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.squadtechs.markhor.foodapp.R
+
+class ActivityCustomerAddress : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_address)
+    }
+}
