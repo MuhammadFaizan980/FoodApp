f1e540519705aac6bd5d50205d8cceab90f3af0e
faizan <mohammadfaizan980@gmail.com>
2019-09-18 23:09:32 +0500

    Added empty edit company screens

---
+++ app/src/main/java/com/squadtechs/markhor/foodapp/trader/activity_trader_edit_company_details_first_screen/ActivityTraderEditCompanyDetailsFirstScreen.kt
@@ -0,0 +1,13 @@
+package com.squadtechs.markhor.foodapp.trader.activity_trader_edit_company_details_first_screen
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.squadtechs.markhor.foodapp.R
+
+class ActivityTraderEditCompanyDetailsFirstScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trader_edit_company_details_first_screen)
+    }
+}
---
+++ app/src/main/java/com/squadtechs/markhor/foodapp/trader/activity_trader_edit_company_details_second_screen/ActivityTraderEditCompanyDetailsSecondScreen.kt
@@ -0,0 +1,13 @@
+package com.squadtechs.markhor.foodapp.trader.activity_trader_edit_company_details_second_screen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.squadtechs.markhor.foodapp.R
+
+class ActivityTraderEditCompanyDetailsSecondScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trader_edit_company_details_second_screen)
+    }
+}
