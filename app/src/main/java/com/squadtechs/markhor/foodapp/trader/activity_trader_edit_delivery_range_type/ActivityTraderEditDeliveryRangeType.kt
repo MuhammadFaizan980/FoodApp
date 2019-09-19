a7876d6d688e8123447d9ae67413ddf9d611ade2
faizan <mohammadfaizan980@gmail.com>
2019-09-19 20:39:52 +0500

    Added edit delivery range code and passed data in intent

--- app/src/main/java/com/squadtechs/markhor/foodapp/trader/activity_trader_edit_delivery_range_type/ActivityTraderEditDeliveryRangeType.kt
+++ app/src/main/java/com/squadtechs/markhor/foodapp/trader/activity_trader_edit_delivery_range_type/ActivityTraderEditDeliveryRangeType.kt
@@ -1,10 +1,13 @@
 package com.squadtechs.markhor.foodapp.trader.activity_trader_edit_delivery_range_type
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.squadtechs.markhor.foodapp.R
+import com.squadtechs.markhor.foodapp.trader.activity_trader_edit_company_logo.ActivityTraderEditCompanyLogo
+import com.squadtechs.markhor.foodapp.trader.activity_trader_edit_company_timings.ActivityTraderEditCompanyTimings
 import com.xw.repo.BubbleSeekBar
 
 class ActivityTraderEditDeliveryRangeType : AppCompatActivity(), EditDeliveryRangeContracts.IView {
@@ -53,6 +56,12 @@ class ActivityTraderEditDeliveryRangeType : AppCompatActivity(), EditDeliveryRan
             txtDeliverYes.setBackgroundResource(R.drawable.txt_edit_range_unselected)
             txtDeliverNo.setBackgroundResource(R.drawable.txt_edit_range_selected)
         }
+
+        imgGoBack.setOnClickListener {
+            startActivity(Intent(this, ActivityTraderEditCompanyLogo::class.java))
+            finish()
+        }
+
     }
 
     private fun initViews() {
@@ -69,10 +78,38 @@ class ActivityTraderEditDeliveryRangeType : AppCompatActivity(), EditDeliveryRan
 
     override fun onValidationResult(status: Boolean) {
         if (status) {
-            //TODO: take user to next screen
+            val mIntent = Intent(this, ActivityTraderEditCompanyTimings::class.java)
+
+
+            mIntent.putExtra("company_name", intent!!.extras!!.get("company_name") as String)
+            mIntent.putExtra("company_phone", intent!!.extras!!.get("company_phone") as String)
+            mIntent.putExtra(
+                "company_description",
+                intent!!.extras!!.get("company_description") as String
+            )
+            mIntent.putExtra("company_cuisine", intent!!.extras!!.get("company_cuisine") as String)
+            mIntent.putExtra(
+                "company_logo_uri",
+                intent!!.extras!!.get("company_logo_uri") as String
+            )
+            mIntent.putExtra(
+                "company_coordinates",
+                intent!!.extras!!.get("company_coordinates") as String
+            )
+            mIntent.putExtra("company_deliver", doYouDeliver)
+            mIntent.putExtra("company_delivery_range", deliveryRange)
+            mIntent.putExtra("company_pick_up_info", pickUpInfo)
+            mIntent.putExtra("company_delivery_time", deliveryTime)
+            startActivity(mIntent)
+            finish()
         } else {
             Toast.makeText(this, "Invalid credentials", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    override fun onBackPressed() {
+        startActivity(Intent(this, ActivityTraderEditCompanyLogo::class.java))
+        finish()
     }
 
     override fun onStart() {
