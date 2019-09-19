3fe2940932d2bf7cd57784cb002be685586c1b3a
faizan <mohammadfaizan980@gmail.com>
2019-09-19 12:08:00 +0500

    Added edit company delivery range

---
+++ app/src/main/java/com/squadtechs/markhor/foodapp/trader/activity_trader_edit_delivery_range_type/ActivityTraderEditDeliveryRangeType.kt
@@ -0,0 +1,13 @@
+package com.squadtechs.markhor.foodapp.trader.activity_trader_edit_delivery_range_type
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.squadtechs.markhor.foodapp.R
+
+class ActivityTraderEditDeliveryRangeType : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trader_edit_delivery_range_type)
+    }
+}
