a71eeba3fcc538c7c125a152446ef12bbc76254b
faizan <mohammadfaizan980@gmail.com>
2019-09-24 21:48:14 +0500

    Added code for calculating delivery and items' price

--- app/src/main/java/com/squadtechs/markhor/foodapp/customer/activity_cutomer_cart_items_details/ActivityCustomerCartItemsDetails.kt
+++ app/src/main/java/com/squadtechs/markhor/foodapp/customer/activity_cutomer_cart_items_details/ActivityCustomerCartItemsDetails.kt
@@ -1,6 +1,7 @@
 package com.squadtechs.markhor.foodapp.customer.activity_cutomer_cart_items_details
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,6 +15,10 @@ class ActivityCustomerCartItemsDetails : AppCompatActivity() {
     private lateinit var adapter: CartItemsDetailsDetailsAdapter
     private lateinit var db: DbUtils
     private lateinit var companyID: String
+    private lateinit var txtTotalPrice: TextView
+    private lateinit var txtTotalDeliveryPrice: TextView
+    private var totalPrice: Int = 0
+    private var totalDeliveryPrice: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,12 +32,34 @@ class ActivityCustomerCartItemsDetails : AppCompatActivity() {
         adapter = CartItemsDetailsDetailsAdapter(list, this)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
+        calculateTotalPrice()
+        calculateDeliveryPrice()
+    }
+
+    private fun calculateTotalPrice() {
+        for (i in list) {
+            totalPrice += (i.cart_item_price.toInt()*i.cart_item_quantity.toInt())
+        }
+        txtTotalPrice.text = "AED $totalPrice"
+    }
+
+    private fun calculateDeliveryPrice() {
+        for (i in list) {
+            if (!i.cart_item_delivery_price.equals("") && i.cart_item_delivery_price != null) {
+                totalDeliveryPrice += i.cart_item_delivery_price.toInt()
+            } else {
+                continue
+            }
+        }
+        txtTotalDeliveryPrice.text = "AED $totalDeliveryPrice"
     }
 
     private fun initViews() {
         recyclerView = findViewById(R.id.cart_list)
         list = ArrayList()
         db = DbUtils(this)
+        txtTotalPrice = findViewById(R.id.txt_total)
+        txtTotalDeliveryPrice = findViewById(R.id.txt_delivery_total)
         companyID = intent!!.extras!!.getString("company_id")!!
     }
 
