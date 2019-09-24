2f260d86465d9184116a37e0fd5e1cbc6a75fd92
faizan <mohammadfaizan980@gmail.com>
2019-09-24 23:03:49 +0500

    Changed bottom navigation library

--- app/src/main/java/com/squadtechs/markhor/foodapp/customer/activity_customer_main/ActivityCustomerMain.kt
+++ app/src/main/java/com/squadtechs/markhor/foodapp/customer/activity_customer_main/ActivityCustomerMain.kt
@@ -1,15 +1,16 @@
 package com.squadtechs.markhor.foodapp.customer.activity_customer_main
 
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
-import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem
 import com.squadtechs.markhor.foodapp.R
 import com.squadtechs.markhor.foodapp.customer.Fragments.customer_fragment_around_me.CustomerFragmentAroundMe
 import com.squadtechs.markhor.foodapp.customer.Fragments.customer_fragment_cart.CustomerFragmentCart
@@ -18,50 +19,100 @@ import com.squadtechs.markhor.foodapp.customer.Fragments.customer_fragment_home.
 import com.squadtechs.markhor.foodapp.customer.Fragments.customer_fragment_orders.CustomerFragmentOrders
 import com.squadtechs.markhor.foodapp.customer.Fragments.customer_fragment_profile.CustomerFragmentProfile
 
-class ActivityCustomerMain : AppCompatActivity(),
-    BottomNavigationView.OnNavigationItemSelectedListener, CustomerFoodFragmetnCallback {
-    private lateinit var bottomNavigation: BottomNavigationView
+class ActivityCustomerMain : AppCompatActivity(), CustomerFoodFragmetnCallback {
+    //  private lateinit var bottomNavigation: BottomNavigationView
     private lateinit var fragmentHome: CustomerFragmentHome
     private var isInFoodCompanies: Boolean = false
+    private lateinit var item1: AHBottomNavigationItem
+    private lateinit var item2: AHBottomNavigationItem
+    private lateinit var item3: AHBottomNavigationItem
+    private lateinit var item4: AHBottomNavigationItem
+    private lateinit var item5: AHBottomNavigationItem
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customer_main)
         checkPermissions()
         initViews()
-        setNavigationListener()
+        //       setNavigationListener()
     }
 
-    private fun setNavigationListener() {
-        bottomNavigation.setOnNavigationItemSelectedListener(this)
-    }
+//    private fun setNavigationListener() {
+//        bottomNavigation.setOnNavigationItemSelectedListener(this)
+//    }
 
     private fun initViews() {
         fragmentHome = CustomerFragmentHome()
-        bottomNavigation = findViewById(R.id.bottom_navigation_view)
+//        bottomNavigation = findViewById(R.id.bottom_navigation_view)
         changeFragment(CustomerFragmentHome())
+        createBottomNav()
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.item_home -> {
-                changeFragment(fragmentHome)
+    private fun createBottomNav() {
+        val bottomNavigation: AHBottomNavigation = findViewById(R.id.bottom_navigation)
+        item1 =
+            AHBottomNavigationItem(R.string.home, R.drawable.ic_home, R.color.colorBlack)
+        item2 =
+            AHBottomNavigationItem(R.string.near_me, R.drawable.ic_location, R.color.colorBlack)
+        item3 =
+            AHBottomNavigationItem(R.string.cart, R.drawable.ic_cart, R.color.colorBlack)
+        item4 =
+            AHBottomNavigationItem(R.string.my_orders, R.drawable.ic_notes, R.color.colorBlack)
+        item5 =
+            AHBottomNavigationItem(R.string.profile, R.drawable.ic_person, R.color.colorBlack)
+        bottomNavigation.addItem(item1)
+        bottomNavigation.addItem(item2)
+        bottomNavigation.addItem(item3)
+        bottomNavigation.addItem(item4)
+        bottomNavigation.addItem(item5)
+        bottomNavigation.setTitleState(AHBottomNavigation.TitleState.ALWAYS_SHOW)
+        bottomNavigation.setForceTint(true)
+        bottomNavigation.setInactiveColor(Color.parseColor("#747474"))
+        bottomNavigation.setAccentColor(Color.parseColor("#FF7132"))
+
+        bottomNavigation.setOnTabSelectedListener { position, wasSelected ->
+            when (position) {
+                0 -> {
+                    changeFragment(fragmentHome)
+                }
+                1 -> {
+                    changeFragment(CustomerFragmentAroundMe())
+                }
+                2 -> {
+                    changeFragment(CustomerFragmentCart())
+                }
+                3 -> {
+                    changeFragment(CustomerFragmentOrders())
+                }
+                4 -> {
+                    changeFragment(CustomerFragmentProfile())
+                }
             }
-            R.id.item_location -> {
-                changeFragment(CustomerFragmentAroundMe())
-            }
-            R.id.item_cart -> {
-                changeFragment(CustomerFragmentCart())
-            }
-            R.id.item_orders -> {
-                changeFragment(CustomerFragmentOrders())
-            }
-            R.id.item_profile -> {
-                changeFragment(CustomerFragmentProfile())
-            }
+            true
         }
-        return true
+
     }
+
+//    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+//        when (item.itemId) {
+//            R.id.item_home -> {
+//                changeFragment(fragmentHome)
+//            }
+//            R.id.item_location -> {
+//                changeFragment(CustomerFragmentAroundMe())
+//            }
+//            R.id.item_cart -> {
+//                changeFragment(CustomerFragmentCart())
+//            }
+//            R.id.item_orders -> {
+//                changeFragment(CustomerFragmentOrders())
+//            }
+//            R.id.item_profile -> {
+//                changeFragment(CustomerFragmentProfile())
+//            }
+//        }
+//        return true
+//    }
 
     private fun changeFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
