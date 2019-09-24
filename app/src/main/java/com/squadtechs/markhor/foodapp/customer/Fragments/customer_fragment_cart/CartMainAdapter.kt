cc19098158d37c1f370f172d242f55105cc06969
faizan <mohammadfaizan980@gmail.com>
2019-09-24 19:58:36 +0500

    Added unfinished cart details UI

--- app/src/main/java/com/squadtechs/markhor/foodapp/customer/Fragments/customer_fragment_cart/CartMainAdapter.kt
+++ app/src/main/java/com/squadtechs/markhor/foodapp/customer/Fragments/customer_fragment_cart/CartMainAdapter.kt
@@ -2,12 +2,14 @@ package com.squadtechs.markhor.foodapp.customer.Fragments.customer_fragment_cart
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.squadtechs.markhor.foodapp.R
+import com.squadtechs.markhor.foodapp.customer.activity_cutomer_cart_items_details.ActivityCustomerCartItemsDetails
 
 class CartMainAdapter(private val list: ArrayList<String>, private val context: Context) :
     RecyclerView.Adapter<CartMainAdapter.CartMainHolder>() {
@@ -26,6 +28,14 @@ class CartMainAdapter(private val list: ArrayList<String>, private val context: 
     override fun onBindViewHolder(holder: CartMainHolder, position: Int) {
         val obj = list[position]
         holder.txtOrder.text = "ITEM COLLECTION # ${position + 1}"
+        holder.txtOrder.setOnClickListener {
+            context.startActivity(
+                Intent(
+                    context,
+                    ActivityCustomerCartItemsDetails::class.java
+                ).putExtra("company_id", obj)
+            )
+        }
     }
 
     inner class CartMainHolder(view: View) : RecyclerView.ViewHolder(view) {
---
+++ app/src/main/java/com/squadtechs/markhor/foodapp/customer/activity_cutomer_cart_items_details/ActivityCustomerCartItemsDetails.kt
@@ -0,0 +1,13 @@
+package com.squadtechs.markhor.foodapp.customer.activity_cutomer_cart_items_details
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.squadtechs.markhor.foodapp.R
+
+class ActivityCustomerCartItemsDetails : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_cart_items_details)
+    }
+}
