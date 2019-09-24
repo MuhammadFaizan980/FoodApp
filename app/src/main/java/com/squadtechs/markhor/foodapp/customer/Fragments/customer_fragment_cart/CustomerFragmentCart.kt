79dae5b33206e80fbd9bc34e67aaa8a6089284e5
faizan <mohammadfaizan980@gmail.com>
2019-09-24 15:22:39 +0500

    Added code for fetching cart item collections

--- app/src/main/java/com/squadtechs/markhor/foodapp/customer/Fragments/customer_fragment_cart/CustomerFragmentCart.kt
+++ app/src/main/java/com/squadtechs/markhor/foodapp/customer/Fragments/customer_fragment_cart/CustomerFragmentCart.kt
@@ -1,26 +1,42 @@
 package com.squadtechs.markhor.foodapp.customer.Fragments.customer_fragment_cart
 
-
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.squadtechs.markhor.foodapp.R
+import com.squadtechs.markhor.foodapp.customer.db_utiils.DbUtils
 
-/**
- * A simple [Fragment] subclass.
- */
 class CustomerFragmentCart : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var list: ArrayList<String>
+    private lateinit var adapter: CartMainAdapter
+    private lateinit var mView: View
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.customer_fragment_cart, container, false)
+        mView = inflater.inflate(R.layout.customer_fragment_cart, container, false)
+        initViews()
+        fetchData()
+        return mView
     }
 
+    private fun fetchData() {
+        list = DbUtils(activity!!).getCompanies()
+        adapter = CartMainAdapter(list, activity!!)
+        recyclerView.layoutManager = LinearLayoutManager(activity!!)
+        recyclerView.adapter = adapter
+    }
+
+    private fun initViews() {
+        recyclerView = mView.findViewById(R.id.recycler)
+        list = ArrayList()
+    }
 
 }
