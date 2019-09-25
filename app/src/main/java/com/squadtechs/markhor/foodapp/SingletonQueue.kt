c44a5134712f606d471d34a9f211441b828475e3
faizan <mohammadfaizan980@gmail.com>
2019-09-25 19:47:31 +0500

    Added code for placing orders

---
+++ app/src/main/java/com/squadtechs/markhor/foodapp/SingletonQueue.kt
@@ -0,0 +1,16 @@
+package com.squadtechs.markhor.foodapp
+
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.Volley
+
+class SingletonQueue {
+    companion object {
+        var requestQueue: RequestQueue? = null
+        fun getRequestQueue(context: android.content.Context): RequestQueue {
+            if (requestQueue == null) {
+                requestQueue = Volley.newRequestQueue(context)
+            }
+            return requestQueue!!
+        }
+    }
+}
--- app/src/main/java/com/squadtechs/markhor/foodapp/customer/activity_cutomer_cart_items_details/ActivityCustomerCartItemsDetails.kt
+++ app/src/main/java/com/squadtechs/markhor/foodapp/customer/activity_cutomer_cart_items_details/ActivityCustomerCartItemsDetails.kt
@@ -6,17 +6,28 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
 import com.squadtechs.markhor.foodapp.R
+import com.squadtechs.markhor.foodapp.SingletonQueue
 import com.squadtechs.markhor.foodapp.customer.activity_customer_select_address.ActivityCustomerSelectAddress
 import com.squadtechs.markhor.foodapp.customer.db_utiils.CartUtil
 import com.squadtechs.markhor.foodapp.customer.db_utiils.DbUtils
+import com.squadtechs.markhor.foodapp.main_utils.MainUtils
+import org.json.JSONObject
 
 class ActivityCustomerCartItemsDetails : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
+    private lateinit var btnPlaceOrder: Button
+    private lateinit var edtRequest: EditText
     private lateinit var list: ArrayList<CartUtil>
     private lateinit var adapter: CartItemsDetailsDetailsAdapter
     private lateinit var db: DbUtils
@@ -31,6 +42,7 @@ class ActivityCustomerCartItemsDetails : AppCompatActivity() {
     private lateinit var pref: SharedPreferences
     private var deliverToMe: String = "yes"
     private var totalPrice: Int = 0
+    private var iteration: Int = 0
     private var totalDeliveryPrice: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,6 +63,7 @@ class ActivityCustomerCartItemsDetails : AppCompatActivity() {
     }
 
     private fun setListeners() {
+        iteration = (list.size - 1)
         txtDeliverToMe.setOnClickListener {
             deliverToMe = "yes"
             txtDeliverToMe.setBackgroundResource(R.drawable.tab_back_selected)
@@ -66,6 +79,69 @@ class ActivityCustomerCartItemsDetails : AppCompatActivity() {
             startActivityForResult(Intent(this, ActivityCustomerSelectAddress::class.java), 88)
         }
 
+        btnPlaceOrder.setOnClickListener {
+            if (edtRequest.text.toString().trim().equals("")) {
+                val dialog = MainUtils.getMessageDialog(
+                    this,
+                    "Information!",
+                    "Are you sure you want to proceed without any allergy request?",
+                    false
+                )
+                dialog.setPositiveButton("Proceed") { dialogInterface, i ->
+                    placeOrder(iteration)
+                }
+                    .setNegativeButton("Cancel") { dialogInterface, i ->
+                        dialogInterface.dismiss()
+                    }
+                dialog.show()
+            } else {
+                placeOrder(iteration)
+            }
+        }
+
+    }
+
+
+    private fun placeOrder(itr: Int) {
+        if (itr >= 0) {
+            val API = "http://squadtechsolution.com/android/v1/add_order.php"
+            val requestQueue = SingletonQueue.getRequestQueue(this)
+            val stringRequestQueue = object : StringRequest(
+                Method.POST,
+                API,
+                Response.Listener { response ->
+                    Log.i("cart_response", response)
+                    if (JSONObject(response).getString("status").equals(" Order Adding  Successfully")) {
+                        iteration--
+                        placeOrder(iteration)
+                    } else {
+                        Toast.makeText(this, "There was an error", Toast.LENGTH_SHORT).show()
+                    }
+                },
+                Response.ErrorListener { error ->
+                    Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show()
+                }) {
+                override fun getParams(): MutableMap<String, String> {
+                    val map = HashMap<String, String>()
+                    map["customer_id"] = list[iteration].customer_id
+                    map["company_id"] = list[iteration].company_id
+                    map["request"] = edtRequest.text.toString().trim()
+                    map["quantity"] = list[iteration].cart_item_quantity
+                    map["status"] = "pending"
+                    map["address"] = address
+                    map["item_id"] = list[iteration].item_id
+                    map["is_food"] = list[iteration].is_food
+                    map["i_will_pick"] = deliverToMe
+                    return map
+                }
+            }
+            requestQueue.add(stringRequestQueue)
+        } else {
+            if (db.deleteCollection(companyID)) {
+                TODO("start main activity in such a way that cart is updated")
+            }
+            return
+        }
     }
 
     private fun calculateTotalPrice() {
@@ -90,6 +166,8 @@ class ActivityCustomerCartItemsDetails : AppCompatActivity() {
     }
 
     private fun initViews() {
+        edtRequest = findViewById(R.id.edt_request)
+        btnPlaceOrder = findViewById(R.id.btn_place_order)
         txtIWillCollect = findViewById(R.id.i_will_collect)
         txtDeliverToMe = findViewById(R.id.txt_deliver_to_me)
         recyclerView = findViewById(R.id.cart_list)
