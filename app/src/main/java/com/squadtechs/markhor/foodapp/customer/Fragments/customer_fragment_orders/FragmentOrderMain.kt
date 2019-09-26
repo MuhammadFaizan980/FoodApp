1b678a5c27b67158cc12e406daa94d87eb81d03e
faizan <mohammadfaizan980@gmail.com>
2019-09-26 12:43:09 +0500

    Added code for getting user current and past orders

---
+++ app/src/main/java/com/squadtechs/markhor/foodapp/customer/Fragments/customer_fragment_orders/FragmentOrderMain.kt
@@ -0,0 +1,76 @@
+package com.squadtechs.markhor.foodapp.customer.Fragments.customer_fragment_orders
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.squadtechs.markhor.foodapp.R
+import com.squadtechs.markhor.foodapp.SingletonQueue
+import com.squadtechs.markhor.foodapp.main_utils.MainUtils
+
+class FragmentOrderMain : Fragment() {
+
+    private lateinit var mView: View
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var txt: TextView
+    private lateinit var list: ArrayList<String>
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mView = inflater.inflate(R.layout.fragment_fragment_order_main, container, false)
+        initViews()
+        populateRecyclerView(arguments!!.getInt("key"))
+        return mView
+    }
+
+    private fun populateRecyclerView(key: Int) {
+        val pd = MainUtils.getLoadingDialog(activity!!, "Loading", "Please wait", false)
+        pd.show()
+        val API = "http://squadtechsolution.com/android/v1/get_customer_order.php"
+        val map = HashMap<String, String>()
+        map["customer_id"] = activity!!.getSharedPreferences(
+            "user_credentials",
+            Context.MODE_PRIVATE
+        ).getString("id", "none")!!
+        when (key) {
+            0 -> {
+                map["is_complete"] = ""
+            }
+            1 -> {
+                map["is_complete"] = "yes"
+            }
+        }
+        val requestQueue = SingletonQueue.getRequestQueue(activity!!)
+        val stringRequest = object : StringRequest(
+            Method.POST,
+            API,
+            Response.Listener { response ->
+                pd.cancel()
+                Log.i("customer_order_response", response)
+            },
+            Response.ErrorListener { error ->
+                pd.cancel()
+                Toast.makeText(activity!!, error.toString(), Toast.LENGTH_LONG).show()
+                Log.i("order_error", error.toString())
+            }) {
+            override fun getParams(): MutableMap<String, String> = map
+        }
+        requestQueue.add(stringRequest)
+    }
+
+    private fun initViews() {
+        recyclerView = mView.findViewById(R.id.main_list)
+        list = ArrayList()
+    }
+
+}
---
+++ app/src/main/java/com/squadtechs/markhor/foodapp/customer/Fragments/customer_fragment_orders/OrderMainAdapter.kt
@@ -0,0 +1,39 @@
+package com.squadtechs.markhor.foodapp.customer.Fragments.customer_fragment_orders
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squadtechs.markhor.foodapp.R
+
+class OrderMainAdapter(
+    private val list: ArrayList<String>, private val context: Context
+) :
+    RecyclerView.Adapter<OrderMainAdapter.CartMainHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartMainHolder =
+        CartMainHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.cart_main_row_design, parent, false
+            )
+        )
+
+    override fun getItemCount(): Int = list.size
+
+    override fun getItemViewType(position: Int): Int = position
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: CartMainHolder, position: Int) {
+        val obj = list[position]
+        holder.txtOrder.text = "COLLECTION # ${position + 1}"
+        holder.txtOrder.setOnClickListener {
+            //TODO: start order main adapter
+        }
+    }
+
+    inner class CartMainHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val txtOrder: TextView = view.findViewById(R.id.txt_item)
+    }
+}
