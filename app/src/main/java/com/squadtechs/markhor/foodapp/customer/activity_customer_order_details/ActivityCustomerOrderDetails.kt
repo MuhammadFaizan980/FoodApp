0543e575e9ea57e134f71374caf45071eb6398e0
faizan <mohammadfaizan980@gmail.com>
2019-09-27 11:12:28 +0500

    Added order details screen for customers

---
+++ app/src/main/java/com/squadtechs/markhor/foodapp/customer/activity_customer_order_details/ActivityCustomerOrderDetails.kt
@@ -0,0 +1,13 @@
+package com.squadtechs.markhor.foodapp.customer.activity_customer_order_details
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.squadtechs.markhor.foodapp.R
+
+class ActivityCustomerOrderDetails : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_order_details)
+    }
+}
